4997a65c04f650e516f1367d648ac96644737625
mYasin <36346577+UYasin@users.noreply.github.com>
2019-04-08 12:31:10 -0400

    added input bar and text button and description

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
 //import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import 'tachyons';
 import './App.css';
@@ -12,6 +13,7 @@ class App extends Component {
       <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {//<FaceRecognition />
       }
