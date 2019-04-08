a4b2a3b61bfc7708f26d2fe859fae969a2d71ac0
mYasin <36346577+UYasin@users.noreply.github.com>
2019-04-08 14:55:24 -0400

    added particles dynamic background

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
+import Particles from 'react-particles-js';
 //import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import 'tachyons';
 import './App.css';
+
+const particleOptions = {
+ particles: {
+  number: {
+    value: 100,
+      density: {
+        enable: true,
+          value_area: 800
+    }
+  }
+ }
+              }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles'
+              params={particleOptions}
+            />
       <Navigation />
       <Logo />
       <Rank />
